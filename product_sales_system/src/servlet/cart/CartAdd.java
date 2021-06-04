509716706bd783ccebb674e0b22943459de7477d
m-motegi <m_motegi@axis-net.co.jp>
2021-06-04 17:42:01 +0900

    a

--- product_sales_system/src/servlet/cart/CartAdd.java
+++ product_sales_system/src/servlet/cart/CartAdd.java
@@ -19,16 +19,6 @@ import bean.Product;
  */
 @WebServlet("/CartAdd")
 public class CartAdd extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartAdd() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
